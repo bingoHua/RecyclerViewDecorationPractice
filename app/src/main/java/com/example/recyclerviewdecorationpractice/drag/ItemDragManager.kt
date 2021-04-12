ef3b948c07cd8e8b9c63ffe17d43627980893124
lizehua <lizehua@insta360.com>
2021-04-12 14:01:09 +0800

    set invisible when item is dragging

--- app/src/main/java/com/example/recyclerviewdecorationpractice/drag/ItemDragManager.kt
+++ app/src/main/java/com/example/recyclerviewdecorationpractice/drag/ItemDragManager.kt
@@ -4,6 +4,7 @@ import android.os.Handler
 import android.os.Looper
 import android.view.GestureDetector
 import android.view.MotionEvent
+import android.view.View
 import android.view.ViewConfiguration
 import androidx.core.view.GestureDetectorCompat
 import androidx.recyclerview.widget.RecyclerView
@@ -18,40 +19,52 @@ class ItemDragManager constructor(private val recyclerView: RecyclerView) {
     private var downPositionY = 0f
     private val handler: Handler = Handler(Looper.getMainLooper())
     private var dragDecoration: DragDecoration? = null
-
+    private val viewConfiguration = ViewConfiguration.get(recyclerView.context)
+    private var findChildViewHolderUnderWithoutTranslation: RecyclerView.ViewHolder? = null
     private var mLongPressed = Runnable {
         isLongPressed = true
     }
     private var isDragging = false
 
-    private val gestureDetector: GestureDetectorCompat = GestureDetectorCompat(recyclerView.context, object : GestureDetector.OnGestureListener {
-        override fun onDown(e: MotionEvent?): Boolean {
-            return false
-        }
+    private val gestureDetector: GestureDetectorCompat =
+        GestureDetectorCompat(recyclerView.context, object : GestureDetector.OnGestureListener {
+            override fun onDown(e: MotionEvent?): Boolean {
+                return false
+            }
 
-        override fun onShowPress(e: MotionEvent?) {
+            override fun onShowPress(e: MotionEvent?) {
 
-        }
+            }
 
-        override fun onSingleTapUp(e: MotionEvent?): Boolean {
-            return false
+            override fun onSingleTapUp(e: MotionEvent?): Boolean {
+                return false
 
-        }
+            }
 
-        override fun onScroll(e1: MotionEvent?, e2: MotionEvent?, distanceX: Float, distanceY: Float): Boolean {
-            return false
+            override fun onScroll(
+                e1: MotionEvent?,
+                e2: MotionEvent?,
+                distanceX: Float,
+                distanceY: Float
+            ): Boolean {
+                return false
 
-        }
+            }
 
-        override fun onLongPress(e: MotionEvent?) {
-            isLongPressed = true
-        }
+            override fun onLongPress(e: MotionEvent?) {
+                isLongPressed = true
+            }
 
-        override fun onFling(e1: MotionEvent?, e2: MotionEvent?, velocityX: Float, velocityY: Float): Boolean {
-            return false
-        }
+            override fun onFling(
+                e1: MotionEvent?,
+                e2: MotionEvent?,
+                velocityX: Float,
+                velocityY: Float
+            ): Boolean {
+                return false
+            }
 
-    })
+        })
 
     init {
         itemTouchListener = object : RecyclerView.OnItemTouchListener {
@@ -60,10 +73,15 @@ class ItemDragManager constructor(private val recyclerView: RecyclerView) {
                     MotionEvent.ACTION_DOWN -> {
                         downPositionX = e.x
                         downPositionY = e.y
-                        handler.postDelayed(mLongPressed, ViewConfiguration.getLongPressTimeout().toLong())
+                        handler.postDelayed(
+                            mLongPressed,
+                            ViewConfiguration.getLongPressTimeout().toLong()
+                        )
                     }
                     MotionEvent.ACTION_MOVE -> {
-                        handler.removeCallbacks(mLongPressed)
+                        if (downPositionX - e.x > viewConfiguration.scaledTouchSlop || downPositionY - e.y > viewConfiguration.scaledTouchSlop) {
+                            handler.removeCallbacks(mLongPressed)
+                        }
                     }
                     MotionEvent.ACTION_UP -> {
                     }
@@ -80,11 +98,9 @@ class ItemDragManager constructor(private val recyclerView: RecyclerView) {
                     MotionEvent.ACTION_MOVE -> {
                         offsetX = e.x - downPositionX
                         offsetY = e.y - downPositionY
-                        checkAndStartDragging(offsetX, offsetY)
+                        checkAndStartDragging(0f, offsetY)
                     }
                     MotionEvent.ACTION_UP -> {
-                        isLongPressed = false
-                        isDragging = false
                         finishDragging()
                     }
                 }
@@ -100,10 +116,17 @@ class ItemDragManager constructor(private val recyclerView: RecyclerView) {
 
     private fun checkAndStartDragging(x: Float, y: Float) {
         if (isLongPressed && !isDragging) {
-            findChildViewHolderUnderWithoutTranslation(recyclerView, downPositionX, downPositionY)?.let {
+            findChildViewHolderUnderWithoutTranslation =
+                findChildViewHolderUnderWithoutTranslation(
+                    recyclerView,
+                    downPositionX,
+                    downPositionY
+                )
+            findChildViewHolderUnderWithoutTranslation?.let {
                 dragDecoration = DragDecoration(recyclerView, it)
                 dragDecoration?.startDragging(x, y)
                 isDragging = true
+                it.itemView.visibility = View.INVISIBLE
             }
         } else if (isDragging) {
             dragDecoration?.updatePosition(x, y)
@@ -111,6 +134,9 @@ class ItemDragManager constructor(private val recyclerView: RecyclerView) {
     }
 
     private fun finishDragging() {
+        isLongPressed = false
+        isDragging = false
+        findChildViewHolderUnderWithoutTranslation?.itemView?.visibility = View.VISIBLE
         dragDecoration?.finishDragging()
     }
 
